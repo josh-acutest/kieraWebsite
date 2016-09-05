131077523e738c4c6fdae35057a6179b3a23cc28
josh-acutest <josh.crossley@acutesales.co.uk>
2016-09-05 23:12:19 +0100

    intro to js

---
+++ kieraWebsite/Content/JavascriptFile.js
@@ -0,0 +1,13 @@
+﻿
+$(document).ready(function () {
+    $(".price-box").hover(function(){
+        $(this).css("background-color", "yellow");
+    }, function(){
+        $(this).css("background-color", "pink");
+    });
+});
+
+
+(document).ready(function () {
+    alert("hi");
+});
